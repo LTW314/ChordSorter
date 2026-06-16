8d554bbe63a52acb649410e45288b378c37d85bf
LTW314 <Weberl8@wwu.edu>
2026-06-16 13:45:08 -0700

    is now a class

--- chordSorter.js
+++ chordSorter.js
@@ -1,8 +1,64 @@
-// users known chords
+class chordSorter {
+
+    constructor(songs, chordsKnown){
+        this.chordsKnown = chordsKnown;
+        this.songs = songs;
+        this.sharedChords = [];
+        this.unknownChords = [];
+    }
+
+
+    //add chord to chords known
+    addChordKnown(chord){
+        this.chordsKnown.push(chord);
+    }
+
+    //remove chord from chords known
+    removeChordKnown(chordToRemove){
+        this.chordsKnown = this.chordsKnown.filter(function(chord){
+            return chord != chordToRemove;
+    })}
+
+
+    // returns song item given song title
+    findSongByTitle(songName) {
+        return this.songs.find(function(song) {
+            return song.title.toLowerCase() === songName.toLowerCase();
+        });
+    }
+
+    // given a song title, returns array of missing chords
+    missingChords(songTitle){
+        // finds the dictionary item belonging to songTitle
+        const comparisonSong = this.findSongByTitle(songTitle);
+        // return null if no songtitle
+        if (!comparisonSong) {return null;}
+        // gets list of chords from that song
+        const songChordList = comparisonSong.chords;
+        // list for known and unknown chords
+        const unknown = [];
+        const shared = [];
+        // loop through two lists, add shared chords and unknown chords to lists
+        for (let i = 0; i < songChordList.length; i++){
+            if (this.chordsKnown.includes(songChordList[i])){
+                shared.push(songChordList[i]);
+            } else if (!unknown.includes(songChordList[i])){
+                unknown.push(songChordList[i])
+            }
+        }
+        // return length of unknown (chords not learned)
+        return unknown.length;
+    }
+
+
+
+    //orderedByArtist(songs, artistName){}
+
+}
+
 const chordsKnown = ["A", "C", "D", "Am", "G"];
-// collection of all songs in database
-const songs = [
-    {
+
+const songs = [{
         title: "Stick Season", 
         artist: "Noah Kahan",
         chords: ["A","E", "F#m", "A/E"]
@@ -18,46 +74,10 @@ const songs = [
         title: "Sailor Song", 
         artist: "Gigi Perez",
         chords: ["C","Em", "G"]
-    }
-];
+    }];
 
 
-// returns song item given song title
-function findSongByTitle(songs, songName) {
-    return songs.find(function(song) {
-        return song.title.toLowerCase() === songName.toLowerCase();
-    });
-}
-
-// given a song title, returns array of missing chords
-function missingChords(songTitle, chordsKnown, songs){
-    // finds the dictionary item belonging to songTitle
-    const comparisonSong = findSongByTitle(songs, songTitle);
-    // return null if no songtitle
-    if (!comparisonSong) {return null;}
-    // gets list of chords from that song
-    const songChordList = comparisonSong.chords;
-    // list for known and unknown chords
-    const unknown = [];
-    const shared = [];
-    // loop through two lists, add shared chords and unknown chords to lists
-    for (let i = 0; i < songChordList.length; i++){
-        if (chordsKnown.includes(songChordList[i])){
-            shared.push(songChordList[i]);
-        } else if (!unknown.includes(songChordList[i])){
-            unknown.push(songChordList[i])
-        }
-    }
-    // return length of unknown (chords not learned)
-    return unknown.length;
-}
-
-//returns a list of song titles
-//function orderedByArtist(songs, artistName){}
-
-
-
-
-console.log(missingChords("Stick Season", chordsKnown));
+const sorter = new chordSorter(songs, chordsKnown);
+console.log(sorter.missingChords("Stick Season"));
 
 
