e4df1080bc0d1051ad83508ca04c911f8f1edaa7
LTW314 <Weberl8@wwu.edu>
2026-07-25 15:06:03 -0700

    rest of functions added

--- chordSorter.js
+++ chordSorter.js
@@ -9,12 +9,12 @@ class chordSorter {
     }
 
 
-    //add chord to chords known
+    // add chord to chords known
     addChordKnown(chord){
         this.chordsKnown.add(chord);
     }
 
-    //remove chord from chords known
+    // remove chord from chords known
     removeChordKnown(chordToRemove){
         this.chordsKnown.delete(chordToRemove);
     }
@@ -26,6 +26,7 @@ class chordSorter {
             return song.title.toLowerCase() === songName.toLowerCase();
         });
     }
+    
 
     // returns array of all song items from an artist
     listSongsByArtist(artistName){
@@ -33,7 +34,20 @@ class chordSorter {
             song.artist.toLowerCase() === artistName.toLowerCase());
     }
 
-    // given a song title, returns array of missing chords
+    // compares an array of chords to known set of chords
+    // returns # missing
+    compareListMissing(givenList, knownSet) {
+        let numMissing = 0;
+        for (let i = 0; i < givenList.length; i++){
+            if (!knownSet.has(givenList[i])){
+                numMissing++;
+            }
+        }
+        return numMissing;
+    }
+
+
+    // given a song title, returns num of missing chords
     missingChords(songTitle){
         // finds the dictionary item belonging to songTitle
         const comparisonSong = this.findSongByTitle(songTitle);
@@ -46,7 +60,7 @@ class chordSorter {
         const shared = [];
         // loop through two lists, add shared chords and unknown chords to lists
         for (let i = 0; i < songChordList.length; i++){
-            if (chordsKnown.has(songChordList[i])){
+            if (this.chordsKnown.has(songChordList[i])){
                 shared.push(songChordList[i]);
             } else if (!unknown.includes(songChordList[i])){
                 unknown.push(songChordList[i])
@@ -59,6 +73,7 @@ class chordSorter {
 
     // given an artist, list songs where all chords are known
     // each song as a list [SongTitle, chordsKnown, Chords Unknown]
+    // equivalent to running unknownByArtist with the value 0
     knownByArtist(artistName){
         
         const artistArray = this.listSongsByArtist(artistName);
@@ -69,7 +84,7 @@ class chordSorter {
             // loop through choreds of each song
             for (let j = 0; j < artistArray[i].chords.length; j++){
                 // mark false if any chords don't match
-                if (!chordsKnown.has(artistArray[i].chords[j])){
+                if (!this.chordsKnown.has(artistArray[i].chords[j])){
                     hasAllChords = false;
                 }
             }
@@ -89,11 +104,102 @@ class chordSorter {
         return songsKnown;
     }
 
-    // given an artist, list songs in order of chords known
+
+    // given an artist, list songs where xUnknown chords are unknown
     // each song as a list [SongTitle, chordsKnown, Chords Unknown]
-    orderedByArtist(artistName){}
+    unknownByArtist(artistName, xUnknown){
+        
+        const artistArray = this.listSongsByArtist(artistName) || [];
+        const songsMatch = []
+
+        // loop through all songs by an artist
+        for (let i = 0; i < artistArray.length; i++){
+            
+            let numUnknown = 0;
+            const unknownChordsList = [];
+            const knownChordsList = [];
+            // loop through choreds of each song
+            for (let j = 0; j < artistArray[i].chords.length; j++){
+                // increase unknown if don't match
+                if (!this.chordsKnown.has(artistArray[i].chords[j])){
+                    numUnknown += 1;
+                    unknownChordsList.push(artistArray[i].chords[j]);
+                } else {
+                    knownChordsList.push(artistArray[i].chords[j]);
+                }
+                // check if numUnkown matches X
+
+            }
+            if (xUnknown === numUnknown){
+                songsMatch.push([]);
+                // new song slot
+                const songSlot = songsMatch[songsMatch.length-1];
+                //title
+                songSlot.push(artistArray[i].title);
+                //chords known
+                songSlot.push(knownChordsList);
+                //chords unknown
+                songSlot.push(unknownChordsList);
+                
+            }
+        }
+        return songsMatch;
+    }
+
+
+    // given an artist string, list all songs in order of # chords unknown
+    // each song as a list [SongTitle, chordsKnown, Chords Unknown]
+    orderedByArtist(artistName){
+        
+        // get all songs by artist
+        const artistList = this.listSongsByArtist(artistName);
+        
+        //get maximum number of chords from list
+        let maxChords = 0;
+        for (let i = 0; i < artistList.length; i++){
+            if (maxChords < artistList[i]["chords"].length){
+                maxChords = artistList[i]["chords"].length;
+            }
+        }
+
+        const sortedArtistList = [];
+
+        for (const song of artistList){
+            const missing = (this.compareListMissing(song["chords"], this.chordsKnown))
+            sortedArtistList[missing] ??= [];
+            sortedArtistList[missing].push(song)
+        }
+        //concatinates, returns
+        return sortedArtistList.flat();
+    }
+
+
+    // given a list of songs items, order by 0 to max by chords known
+    orderedByPlaylist(givenList){
+
+        //get maximum number of chords from list
+        let maxChords = 0;
+        for (let i = 0; i < givenList.length; i++){
+            if (maxChords < givenList[i]["chords"].length){
+                maxChords = givenList[i]["chords"].length;
+            }
+        }
+
+        const sortedList = [];
+
+        for (const song of givenList){
+            const missing = (this.compareListMissing(song["chords"], this.chordsKnown))
+            sortedList[missing] ??= [];
+            sortedList[missing].push(song)
+        }
+        //concatinates, returns
+        return sortedList.flat();
+
+    }
 
 }
+
+// TESTING
 
 const chordsKnown = new Set(["A","E", "F#m", "A/E", "Em"]);
 
@@ -113,12 +219,22 @@ const songs = [{
         title: "Sailor Song", 
         artist: "Gigi Perez",
         chords: ["C","Em", "G"]
-    }];
+    },
+
+    {
+        title: "TestSong",
+        artist: "Noah Kahan",
+        chords: ["A"]
+    }
+
+];
 
 
 
 const sorter = new chordSorter(songs, chordsKnown);
-console.log(sorter.missingChords("Stick Season"));
-console.log(sorter.knownByArtist("Noah Kahan"));
+//console.log(sorter.missingChords("Stick Season"));
+//console.log(sorter.listSongsByArtist("Noah Kahan")[0]["chords"]);
+console.log(sorter.orderedByArtist("Noah Kahan"));
+//console.log(sorter.unknownByArtist("Noah Kahan", 3));
 
 
